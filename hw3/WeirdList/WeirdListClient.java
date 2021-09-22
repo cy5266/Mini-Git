bf587f8391cdf957b5f20dad24f22eed2b26cefe
Cindy Yang <cindycy@berkeley.edu>
2021-09-21 22:25:36 -0700

    final pls

--- hw3/WeirdList/WeirdListClient.java
+++ hw3/WeirdList/WeirdListClient.java
@@ -16,7 +16,7 @@ class WeirdListClient {
         WeirdListSum totalSum = new WeirdListSum();
         WeirdList temp = L.map(totalSum);
         String test = temp.toString();
-        String[] arrOfStr = test.split(" ", test.length()-1);
+        String[] arrOfStr = test.split(" ", -1);
         System.out.println(arrOfStr);
         return Integer.parseInt(arrOfStr[arrOfStr.length-1]);
     }
