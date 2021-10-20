74b9625f167ce4eb65c3226489ba33f1110ee916
Cindy Yang <cindycy@berkeley.edu>
2021-10-19 21:33:55 -0700

    test EC

--- hw6/ECHashStringSet.java
+++ hw6/ECHashStringSet.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 /** A set of String values.
@@ -5,16 +6,90 @@ import java.util.List;
  */
 class ECHashStringSet implements StringSet {
 
+    private ArrayList<String>[] buckets; //array of arraylists
+    private int numBucketSize;
+    private int numItems;
+
+    public ECHashStringSet() {
+
+        numItems = 0;
+        numBucketSize = 10;
+        //(ArrayList<String>[])
+        buckets =  new ArrayList[numBucketSize];
+
+        for (int i = 0; i <buckets.length; i++) {
+            buckets[i] = new ArrayList<String>();
+        }
+    }
+
     @Override
     public void put(String s) {
+        //if we have reached the load limit, we should resize
+        // if (number of items / number of buckets) > 5
+
+        if (numItems / numBucketSize > 5) {
+            resize();
+        }
+
+        //figure out which index i bucket to go to
+        //whichBucket
+
+        int index = whichBucket(s);
+        ArrayList<String> test = buckets[index];
+        test.add(s);
+        buckets[index] = test;
+
+        // get arraylist at bucket[i]
+        //add s to the linkedlist
         // FIXME
+    }
+
+    private void resize() {
+        ArrayList<String>[] buckets2 =  new ArrayList[numBucketSize*2];
+
+        for (int i = 0; i <buckets.length; i++) {
+            buckets[i] = new ArrayList<String>();
+        }
+
+        // take all of the elements from the hashset we already have and figure out
+        // which bucket to put them in the new list
+
+        for (int i = 0; i < buckets.length; i ++) {
+            for (String element: buckets[i]) {
+                int bucketIndex = whichBucket(element);
+                buckets2[bucketIndex].add(element);
+            }
+        }
+
+        buckets = buckets2;
+        //make a new array to move everything to
+        // fill the array with arraylists
+        // take all of the elements from the hashset we already have and figure out
+        // which bucket to put them in the new list
     }
 
     @Override
     public boolean contains(String s) {
-        return false; // FIXME
+
+        int index = whichBucket(s);
+
+        if (buckets[index].contains(s)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        //first figure out which index i bucket s would be in
+        //then get the arraylist at the bucket, and see if the arraylist contains S
+//        return false; // FIXME
     }
 
+    private int whichBucket(String s) {
+        return s.hashCode() % numBucketSize;
+        //returns which bucket i the string should be in
+        //call the default string hashcode, as in s.hashCode()
+        //figure out a way to make that default hashcode wrap or fit within the range 0-numbuckets-1
+    }
     @Override
     public List<String> asList() {
         return null; // FIXME
